44cb7bdcc5651149150d97add95ff2695c36bebb
Mikalai Shchakatsikhin <keep_ya_head_up@inbox.ru>
2022-09-04 02:38:16 -0700

    fix

--- template/src/features/Welcome.tsx
+++ template/src/features/Welcome.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { observer } from 'mobx-react-lite';
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export const Welcome = observer(() => {
   return (
-      <View>
-        <Text>Welcome Screen</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>Welcome to Template</Text>
+    </View>
   );
 });
--- template/src/modules/store.ts
+++ template/src/modules/store.ts
@@ -1,4 +1,4 @@
-import { observable } from 'mobx';
+import { action, observable } from 'mobx';
 import { TStyle } from 'react-native-modalize/lib/options';
 
 type DatePickerConfig = Partial<{
